c75ad591ba9da1cfd7cc78ee86aa6ef2480ca185
Maëlle Nioche <maelle.nioche@gmail.com>
2025-05-28 16:34:31 +0200

    fix: update user identifier to return email instead of username

--- biblios-app/src/Entity/User.php
+++ biblios-app/src/Entity/User.php
@@ -79,7 +79,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     public function getUserIdentifier(): string
     {
-        return (string) $this->username;
+        return (string) $this->email;
     }
 
     /**
