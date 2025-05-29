bd578c9774f30409503391981013fc340601b262
Maëlle Nioche <maelle.nioche@gmail.com>
2025-05-29 10:12:56 +0200

    feat: enhance registration/login forms and customize design
    - Added missing fields to registration form (roles, first name, last name)
    - Roles are now a multiple choice with checkboxes (ROLE_USER, ROLE_ADMIN)
    - Reused layout from main template with reduced column width
    - Translated form labels using Symfony translation system
    - Verified login works with debug toolbar authentication icon

--- biblios-app/src/Form/RegistrationForm.php
+++ biblios-app/src/Form/RegistrationForm.php
@@ -5,7 +5,10 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -17,8 +20,30 @@ class RegistrationForm extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('firstname', TextType::class, [
+                'label' => 'registration.form.first_name',
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'registration.form.last_name',
+            ])
+            ->add('username', TextType::class, [
+                'label' => 'registration.form.username',
+                'required' => false,                
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'registration.form.email',
+            ])
+            ->add('roles', ChoiceType::class, [
+                'label' => 'registration.form.roles.label',
+                'choices' => [
+                    'registration.form.roles.user' => 'ROLE_USER',
+                    'registration.form.roles.admin' => 'ROLE_ADMIN',
+                ],
+                'multiple' => true,
+                'expanded' => true,
+            ])
             ->add('plainPassword', PasswordType::class, [
+                'label' => 'registration.form.password',
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
