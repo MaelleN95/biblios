00b8ee49bde06850cf29ab9eaeb536776aed4dc6
Maëlle Nioche <maelle.nioche@gmail.com>
2025-05-29 10:54:00 +0200

    feat: add role hierarchy for user permissions and enforce admin access in AuthorController

--- biblios-app/src/Controller/Admin/AuthorController.php
+++ biblios-app/src/Controller/Admin/AuthorController.php
@@ -3,19 +3,21 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Author;
+use Pagerfanta\Pagerfanta;
 use App\Form\AuthorTypeForm;
 use App\Repository\AuthorRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Pagerfanta\Doctrine\ORM\QueryAdapter;
-use Pagerfanta\Pagerfanta;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/admin/author')]
 class AuthorController extends AbstractController
 {
+    #[IsGranted('IS_AUTHENTICATED')]
     #[Route('', name: 'app_admin_author_index', methods: ['GET'])]
     public function index(Request $request, AuthorRepository $repository): Response
     {
@@ -45,6 +47,10 @@ class AuthorController extends AbstractController
     #[Route('/{id}/edit', name: 'app_admin_author_edit', requirements: ['id' => '\d+'], methods: ['GET', 'POST'])]
     public function new(?Author $author, Request $request, EntityManagerInterface $manager): Response
     {
+        if (null === $author) {
+            $this->denyAccessUnlessGranted('ROLE_ADMIN');
+        }
+        
         $author ??= new Author();
         $form = $this->createForm(AuthorTypeForm::class, $author);
 
